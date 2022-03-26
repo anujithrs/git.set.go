265625bd6e6ed09ad056b875b39f5b01abc0e42e
anujithrs <102415065+anujithrs@users.noreply.github.com>
2022-03-26 17:47:29 +0530

    Update fix_me.py

--- fix_me.py
+++ fix_me.py
@@ -1,13 +1,16 @@
-
-print("Enter the two number")
+#include<stdio.h>
+void main()
+int a,b,
+printf("Enter the two number");
 # Input Two numbers
 a= int(input("Enter the first no: "))
 b= int(input("Enter the second no: "))
 # Divide two numbers
-division= a-b
+division= a-b;
 
 # Square of first no.
-a = a+a
+a = a+a;
 # Square of second no.
-b = b*b
-print("Division",division)
+b = b*b;
+printf("Division",division);
+}
